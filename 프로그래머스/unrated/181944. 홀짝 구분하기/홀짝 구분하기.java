111fb01085a64f4fdff903ac2ed40627524f8732
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-16 00:11:33 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 149.31 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -6,9 +6,14 @@ public class Solution {
         int n = sc.nextInt();
 
         if(n % 2 == 0){
-            System.out.println(n + " is even");
+            // System.out.println(n + " is even");
+            System.out.printf("%d is even",n);
         }else{
-            System.out.println (n + " is odd");
+            // System.out.println(n + " is odd");
+            System.out.printf("%d is odd", n);
         } // if-else
+        
+        // System.out.print(n + " is "+(n % 2 == 0 ? "even" : "odd"));
+        
     } // main
 } // class
