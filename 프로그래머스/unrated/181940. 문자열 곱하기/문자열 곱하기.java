ec3594d08a2b53d66d7a1957af290d1020311223
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-17 22:23:57 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -11,14 +11,21 @@
 // } // class
 
 
+// class Solution {
+//     public String solution(String my_string, int k) {     
+//         StringBuilder sb = new StringBuilder();
+        
+//         for(int i = 0; i < k; i++){
+//             sb.append(my_string);
+//         } // for
+        
+//         return sb.toString();
+//     } // main
+// } // class
+
+
 class Solution {
-    public String solution(String my_string, int k) {     
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = 0; i < k; i++){
-            sb.append(my_string);
-        } // for
-        
-        return sb.toString();
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
     } // main
 } // class
