d78b2fcd46206abfd86476300f6b182df26a8031
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-18 23:43:53 +0900

    [level unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int a, int b, boolean flag) {
        
-        if(flag){
-            return a + b;
-        } else{
-            return a - b;
-        } // if-else
+        // if(flag){
+        //     return a + b;
+        // } else{
+        //     return a - b;
+        // } // if-else
+        
+        return flag ? a + b : a - b;
         
     } // main
 } // class
