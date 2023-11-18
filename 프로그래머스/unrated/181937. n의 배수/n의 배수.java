9aa60c394807bc75150371e23a6de6626c4d0bc2
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-18 23:46:27 +0900

    [level unrated] Title: n의 배수, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int n) {
+        
+        // if(num%n == 0){
+        //     return 1;
+        // } else {
+        //     return 0;
+        // } // if-else
+        
+        return (num%n==0) ? 1 : 0;
+    } // main
+} // class
