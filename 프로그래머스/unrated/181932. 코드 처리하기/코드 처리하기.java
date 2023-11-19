4b970202e346490470cdfcd7f425bb4552a37aca
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-19 17:41:10 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder sb = new StringBuilder();
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            } // if
+
+            if (i % 2 == mode) {
+                sb.append(current);
+            } // if
+        } // for
+        
+        return sb.length() == 0 ? "EMPTY" : sb.toString();
+    } // main
+} // calss
