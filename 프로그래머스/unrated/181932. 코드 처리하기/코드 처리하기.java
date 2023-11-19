df443d61b58a5494ca7a23d65d23cdf5d0474d04
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-19 20:51:02 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -1,19 +1,44 @@
+// class Solution {
+//     public String solution(String code) {
+//         StringBuilder sb = new StringBuilder();
+//         int mode = 0;
+//         for (int i = 0; i < code.length(); i++) {
+//             char current = code.charAt(i);
+//             if (current == '1') {
+//                 mode = mode == 0 ? 1 : 0;
+//                 continue;
+//             } // if
+
+//             if (i % 2 == mode) {
+//                 sb.append(current);
+//             } // if
+//         } // for
+        
+//         return sb.length() == 0 ? "EMPTY" : sb.toString();
+//     } // main
+// } // calss
+
+
 class Solution {
     public String solution(String code) {
-        StringBuilder sb = new StringBuilder();
         int mode = 0;
-        for (int i = 0; i < code.length(); i++) {
-            char current = code.charAt(i);
-            if (current == '1') {
-                mode = mode == 0 ? 1 : 0;
-                continue;
-            } // if
+        String ret = "";
 
-            if (i % 2 == mode) {
-                sb.append(current);
-            } // if
+        for (int idx = 0; idx < code.length(); idx++) {
+            if (code.charAt(idx) == '1') {
+                mode = 1 - mode;
+            } else if (mode == 0 && idx % 2 == 0) {
+                ret += code.charAt(idx);
+            } else if (mode == 1 && idx % 2 == 1) {
+                ret += code.charAt(idx);
+            } // else-if
         } // for
         
-        return sb.length() == 0 ? "EMPTY" : sb.toString();
+        if (ret.equals("")) {
+            return "EMPTY";
+        } else {
+            return ret;
+        } // if-else
+        
     } // main
-} // calss
+} // class
