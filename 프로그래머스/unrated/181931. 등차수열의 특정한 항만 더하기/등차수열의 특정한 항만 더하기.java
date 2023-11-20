d3e7f5c2d80f5a5b0f424e2578f4a05f26c2bf11
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-20 23:57:30 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -16,3 +16,18 @@ class Solution {
         return answer;
     } //main
 } //class
+
+
+// class Solution {
+//     public int solution(int a, int d, boolean[] included) {
+//         int answer = 0;
+
+//         for(int i = 0; i < included.length; i++){
+//             if(included[i]){
+//                 answer +=  a + (d*i);
+//             } // if
+//         } // for
+
+//         return answer;
+//     } // main
+// } // class
