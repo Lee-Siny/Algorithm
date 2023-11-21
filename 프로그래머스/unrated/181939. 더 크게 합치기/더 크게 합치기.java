0755e9768a555cf986808b345eecd99d3660ffe0
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-21 23:16:11 +0900

    [level unrated] Title: 더 크게 합치기, Time: 8.73 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,36 @@
+// class Solution {
+//     public int solution(int a, int b) {        
+//         String ab = String.valueOf(a) + String.valueOf(b);
+//         String ba = String.valueOf(b) + String.valueOf(a);
+        
+//         int abValue = Integer.parseInt(ab);
+//         int baValue = Integer.parseInt(ba);
+        
+//         if (abValue >= baValue) {
+//             return abValue;
+//         } else {
+//             return baValue;
+//         } // if=else
+//     } // main
+// } // class
+
+
+// class Solution {
+//     public int solution(int a, int b) {
+//         int ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+//         int ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+//         return ab >= ba ? ab : ba;
+//     } // main
+// } // class
+
+
 class Solution {
-    public int solution(int a, int b) {        
-        String ab = String.valueOf(a) + String.valueOf(b);
-        String ba = String.valueOf(b) + String.valueOf(a);
-        
-        int abValue = Integer.parseInt(ab);
-        int baValue = Integer.parseInt(ba);
-        
-        if (abValue >= baValue) {
-            return abValue;
-        } else {
-            return baValue;
-        } // if=else
+    public int solution(int a, int b) {
+        int answer = 0;
+        int aLong = Integer.parseInt(""+a+b);
+        int bLong = Integer.parseInt(""+b+a);
+        answer = aLong > bLong ? aLong : bLong;
+
+        return answer;
     } // main
 } // class
