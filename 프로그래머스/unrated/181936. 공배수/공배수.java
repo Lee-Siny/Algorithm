56631ff0c7f6cbbcda81c60c091bf6fc60234173
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-21 23:16:58 +0900

    [level unrated] Title: 공배수, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,17 +1,18 @@
 class Solution {
     public int solution(int number, int n, int m) {
         
-        if(number%n==0){
+//         if(number%n==0){
             
-            if(number%m==0){
-                return 1;
-            } else {
-            return 0;    
-            } // if-else
+//             if(number%m==0){
+//                 return 1;
+//             } else {
+//             return 0;    
+//             } // if-else
             
-        } else {
-            return 0;    
-        }  // if-else
+//         } else {
+//             return 0;    
+//         }  // if-else
         
+        return number % n == 0 && number % m == 0 ? 1 : 0;
     } // main
 } // class
