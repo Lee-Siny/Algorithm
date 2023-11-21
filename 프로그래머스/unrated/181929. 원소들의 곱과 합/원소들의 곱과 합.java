00205a89f38a0ef188481c5900c54b7712e4a74a
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-21 23:10:29 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public int solution(int[] num_list) {
+//         int sum = 0;
+//         int mult = 1;
+        
+//         for(int i = 0; i < num_list.length; i++){
+//             sum += num_list[i];
+//             mult *= num_list[i];            
+//         } //for
+        
+//         if(mult < Math.pow(sum, 2)){
+//             return 1;
+//         } else{
+//             return 0;
+//         } //if-else
+//     } //main
+// } //class
+
+
 class Solution {
     public int solution(int[] num_list) {
-        int sum = 0;
-        int mul = 1;
-        
-        for(int i = 0; i < num_list.length; i++){
-            sum += num_list[i];
-            mul *= num_list[i];            
+        int sum  = 0;
+        int mult = 1;
+
+        for(int i : num_list) {
+            sum += i;
+            mult *= i;
         } //for
-        
-        if(mul < Math.pow(sum, 2)){
-            return 1;
-        } else{
-            return 0;
-        } //if-else
-    } //main
+
+        return Math.pow(sum,2) > mult ? 1 : 0;
+    } // main
 } //class
