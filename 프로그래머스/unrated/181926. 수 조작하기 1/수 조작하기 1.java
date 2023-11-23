88a22e003cea78198dd33b6521119c85ba82849a
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-23 23:55:04 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.16 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,17 +1,45 @@
+// class Solution {
+//     public int solution(int n, String control) {
+//         for(char c : control.toCharArray()){
+//             if(c == 'w'){
+//                 n += 1;
+//             } else if(c == 's') {
+//                 n -= 1;
+//             } else if(c == 'd') {
+//                 n += 10;
+//             } else if(c == 'a') {
+//                 n -= 10;
+//             } // else-if
+//         } // for
+        
+//         return n;
+//     } //main
+// } //class
+
+
+// class Solution {
+//     public int solution(int n, String control) {
+//         int answer = n;
+
+//         for(char c : control.toCharArray()) {
+//             switch(c) {
+//                 case 'w': answer += 1; break;
+//                 case 's': answer -= 1; break;
+//                 case 'd': answer += 10; break;
+//                 case 'a': answer -= 10; break;
+//                 default:break;
+//             } //switch
+//         } //for
+//         return answer;
+//     } //main
+// } //class
+
+
 class Solution {
     public int solution(int n, String control) {
         for(char c : control.toCharArray()){
-            if(c == 'w'){
-                n += 1;
-            } else if(c == 's') {
-                n -= 1;
-            } else if(c == 'd') {
-                n += 10;
-            } else if(c == 'a') {
-                n -= 10;
-            } // else-if
-        } // for
-        
+            n += c == 'w' ? 1 : c == 's' ? -1 : c == 'd' ? 10 : -10;
+        } //for
         return n;
     } //main
 } //class
