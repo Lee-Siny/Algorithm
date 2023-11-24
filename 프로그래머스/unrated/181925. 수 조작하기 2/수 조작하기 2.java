31e15ee8822cb1116e0974140fdf4a6e776cd0b8
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-24 19:10:50 +0900

    [level unrated] Title: 수 조작하기 2, Time: 749.19 ms, Memory: 385 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,13 +1,30 @@
+// class Solution {
+//     public String solution(int[] numLog) {
+//         String answer = "";
+        
+//         for(int i = 0; i < numLog.length-1 ; i++){
+//             int num = numLog[i + 1] - numLog[i];
+            
+//             answer += num == 1 ? "w" : num == -1 ? "s" : num == 10 ? "d" : "a";
+//         } //for
+        
+//         return answer;
+//     } //main
+// } //class
+
+
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        
-        for(int i = 0; i < numLog.length-1 ; i++){
-            int num = numLog[i + 1] - numLog[i];
-            
-            answer += num == 1 ? "w" : num == -1 ? "s" : num == 10 ? "d" : "a";
+        for(int i=1; i<numLog.length; i++){
+            int num = numLog[i-1] - numLog[i];
+            switch(num){
+                case -1 : answer+='w'; break;
+                case 1 : answer+='s'; break;
+                case -10 : answer+='d'; break;
+                case 10 : answer+='a'; break;
+            } //switch
         } //for
-        
         return answer;
     } //main
 } //class
