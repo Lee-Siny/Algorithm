0db24098e6aa93b5f4a77c8672662815a6707830
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-11-27 18:15:28 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i = 0; i < queries.length; i++){
+            int n = arr[queries[i][0]];
+            
+            arr[queries[i][0]] = arr[queries[i][1]];
+            arr[queries[i][1]] = n;
+        } //for
+        
+        return arr;
+    } //main
+} //class
