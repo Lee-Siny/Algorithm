cfaf43a68b0554bbe82e26232ed656098249fea8
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-01 23:54:17 +0900

    [Bronze V] Title: 꼬마 정민, Time: 196 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        long C = sc.nextLong();
-        
-        System.out.println(A + B + C);
 
+        String str = sc.nextLine();
+        String[] numbers = str.split(" ");
+
+        long A = Long.parseLong(numbers[0]);
+        long B = Long.parseLong(numbers[1]);
+        long C = Long.parseLong(numbers[2]);
+
+        System.out.println(A + B + C);
         sc.close();
     } //main
 } //class
