5cfe3cc657e29139867a2220e8ff50a1282a4f4f
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-02 00:05:51 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//        Java13 부터 삼중큰따옴표로 텍스트블록 사용가능
+//        String cat = """
+//                \\    /\\
+//                 )  ( ')
+//                (  /  )
+//                 \\(__)|
+//                """;
+//        System.out.println(cat);
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    } //main
+} //class
