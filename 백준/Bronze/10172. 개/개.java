732143a6e071da1615c9380f582b1b1e078cd693
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-02 00:12:49 +0900

    [Bronze V] Title: 개, Time: 108 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//        Java13 부터 삼중큰따옴표로 텍스트블록 사용가능
+//        String dog = """
+//                |\\_/|
+//                |q p|   /}
+//                ( 0 )""\"\\
+//                |"^"`    |
+//                ||_/=\\\\__|
+//                """;
+//        System.out.println(dog);
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+    } //main
+} //class
