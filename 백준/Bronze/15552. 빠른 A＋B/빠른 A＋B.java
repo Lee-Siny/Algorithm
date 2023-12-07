6671961388daff6085036ef88858986dcde5395a
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-07 22:49:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 856 ms, Memory: 235696 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        int a;
+        int b;
+
+        for(int i = 0 ; i < T ; i++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            bw.write((a + b) + "\n");
+        } //for
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    } //main
+} //class
