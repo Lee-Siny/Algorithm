0cdc4b5760ef8e35fb02badd70f977eccc4c2dac
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-08 22:46:37 +0900

    Update A＋B － 8.java

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,11 +1,36 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        StringTokenizer st;
+//
+//        int t = Integer.parseInt(br.readLine());
+//
+//        for(int c = 1 ; c <= t ; c++){
+//            st = new StringTokenizer(br.readLine());
+//
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//
+//            bw.write(String.format("Case #%d: %d + %d = %d \n", c, a, b, a + b));
+//        } //for
+//
+//        br.close();
+//
+//        bw.flush();
+//        bw.close();
+//    } //main
+//} //class
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
@@ -16,12 +41,11 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            bw.write(String.format("Case #%d: %d + %d = %d \n", c, a, b, a + b));
+            sb.append(String.format("Case #%d: %d + %d = %d \n", c, a, b, a + b));
         } //for
 
+        System.out.println(sb);
+        
         br.close();
-
-        bw.flush();
-        bw.close();
     } //main
 } //class
