8c2dd617d11ceab6acdc490f00dd998c0ebf7e8d
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-19 23:45:16 +0900

    [Bronze V] Title: 문자와 문자열, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine()) - 1;
+
+        System.out.println(str.charAt(n));
+
+        br.close();
+    } //main
+} //class
