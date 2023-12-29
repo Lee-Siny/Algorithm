5bce1cc670097dbc3c2cdfa1f364f7a7b70aeb25
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-29 23:49:28 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -2,6 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//        // 별 증가 횟수 => 5번
+//        for(int i = 1; i <= n; i++){
+//
+//            // (n - i) 만큼 공백을 앞에 넣음
+//            for(int j = 1; j <= (n - i); j++){
+//                sb.append(" ");
+//            } //for(공백)
+//
+//            // (i * 2 -  1) 만큼 *을 넣음
+//            for(int k = 1; k <= (i * 2 - 1); k++){
+//                sb.append("*");
+//            } //for(별)
+//
+//            // 줄 바꿈
+//            sb.append("\n");
+//        } //for
+//
+//        // 별 감소 횟수 => 4번
+//        for(int i = 1; i <= (n - 1); i++ ){
+//
+//            // i 만큼 공백을 넣음
+//            for(int j = 1; j <= i; j++){
+//                sb.append(" ");
+//            } //for(공백)
+//
+//            // ((n - i) * 2 - 1) 만큼 *을 넣음
+//            for(int k = 1 ; k <= ((n - i) * 2 - 1); k++){
+//                sb.append("*");
+//            } //for(별)
+//
+//            sb.append("\n");
+//        } //for
+//
+//        System.out.println(sb);
+//
+//        br.close();
+//    } //main
+//} //class
+
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,12 +60,12 @@ public class Main {
         // 별 증가 횟수 => 5번
         for(int i = 1; i <= n; i++){
 
-            // (n - i) 만큼 공백을 앞에 넣음
+            // (n - 1) 만큼 공백을 앞에 넣음
             for(int j = 1; j <= (n - i); j++){
                 sb.append(" ");
             } //for(공백)
 
-            // (i * 2 -  1) 만큼 *을 넣음
+            // (i * 2 -1) 만큼 *을 넣음
             for(int k = 1; k <= (i * 2 - 1); k++){
                 sb.append("*");
             } //for(별)
@@ -27,15 +75,15 @@ public class Main {
         } //for
 
         // 별 감소 횟수 => 4번
-        for(int i = 1; i <= (n - 1); i++ ){
+        for(int i = (n - 1); i >= 1 ; i-- ){
 
-            // i 만큼 공백을 넣음
-            for(int j = 1; j <= i; j++){
+            // (n - i) 만큼 공백을 넣음
+            for(int j = 1; j <= (n - i); j++){
                 sb.append(" ");
             } //for(공백)
 
-            // ((n - i) * 2 - 1) 만큼 *을 넣음
-            for(int k = 1 ; k <= ((n - i) * 2 - 1); k++){
+            // (i * 2 - 1) 만큼 *을 넣음
+            for(int k = 1; k <= (i * 2 - 1); k++){
                 sb.append("*");
             } //for(별)
 
