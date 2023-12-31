da8011a274dd6e6333ae963470f935b6e513607d
Sinyoung Lee <sinyoung.siny@gmail.com>
2023-12-31 23:51:07 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21096 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toUpperCase();
+        int[] alphabetArr = new int[26];
+
+        for(int i = 0; i < str.length(); i++){
+            alphabetArr[str.charAt(i) - 'A']++;
+//            alphabetArr[str.charAt(i) - 65]++;
+        } //for
+
+        int max = -1;
+        char ch = '?';
+        for(int j = 0; j < alphabetArr.length; j++){
+            if(max < alphabetArr[j]){
+                max = alphabetArr[j];
+                ch = (char) (j + 65);
+            } else if(max == alphabetArr[j]){
+                ch = '?';
+            } //else-if
+        } //for
+
+        System.out.println(ch);
+
+        br.close();
+    } //main
+} //class
