5b7e2d0ee0f3212e3a51f48d690a375e40061d78
Sinyoung Lee <sinyoung.siny@gmail.com>
2024-01-31 19:02:43 +0900

    [Bronze III] Title: 세 막대, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] arr = {a, b, c};
+
+        Arrays.sort(arr);
+
+        if(arr[0] + arr[1] > arr[2]) {
+            System.out.println(a + b + c);
+        } else {
+            System.out.println((arr[0] + arr[1]) * 2 - 1);
+        } //if-else
+    } //main
+} //class
