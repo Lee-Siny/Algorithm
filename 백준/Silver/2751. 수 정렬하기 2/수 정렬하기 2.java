8b9f81733e03da75dee03db9f54e3ad2023b8f04
Sinyoung Lee <sinyoung.siny@gmail.com>
2024-02-10 19:36:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1440 ms, Memory: 130104 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        } //for
+
+        Collections.sort(list);
+
+        for(int result : list) {
+            sb.append(result).append("\n");
+        } //for
+
+        System.out.println(sb);
+    } //main
+} //class
