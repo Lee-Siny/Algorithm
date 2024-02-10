0c8f4c55851b542472b6bbdd1b4e4a1c4d4fd22e
Sinyoung Lee <sinyoung.siny@gmail.com>
2024-02-10 19:39:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 776 ms, Memory: 94116 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,51 @@
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        for(int i = 0; i < n; i++) {
+//            list.add(Integer.parseInt(br.readLine()));
+//        } //for
+//
+//        Collections.sort(list);
+//
+//        for(int result : list) {
+//            sb.append(result).append("\n");
+//        } //for
+//
+//        System.out.println(sb);
+//    } //main
+//} //class
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // -1000000 ~ 1000000
+        boolean[] arr = new boolean[2000001];
+
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<>();
-
         for(int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         } //for
 
-        Collections.sort(list);
-
-        for(int result : list) {
-            sb.append(result).append("\n");
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i]) {
+                sb.append((i - 1000000)).append("\n");
+            } //if
         } //for
 
-        System.out.println(sb);
+        System.out.print(sb);
     } //main
 } //class
