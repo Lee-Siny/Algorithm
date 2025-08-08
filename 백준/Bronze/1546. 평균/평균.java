07cdaa9af280703a3cdb931ad549022f9bfc5212
Sinyoung Lee <sinyoung.siny@gmail.com>
2025-08-08 23:06:52 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double[] arr = new double[Integer.parseInt(br.readLine())];
-
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
-        }
         br.close();
-        
-        double sum = 0;
-        Arrays.sort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        int max = 0;
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            max = Math.max(max, score);
+            sum += score;
         }
 
-        System.out.println(sum / arr[arr.length - 1] * 100 / arr.length);
+        System.out.println(sum / max * 100 / n);
     }
 }
