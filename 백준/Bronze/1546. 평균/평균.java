5494d466b4fb3ae0f16ec05629f1de9e1d296c76
Sinyoung Lee <sinyoung.siny@gmail.com>
2025-08-08 23:06:12 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double arr[] = new double[Integer.parseInt(br.readLine())];
+        double[] arr = new double[Integer.parseInt(br.readLine())];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         
-        for(int i = 0 ; i < arr.length ; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Double.parseDouble(st.nextToken());
-        } //for
-
-        Arrays.sort(arr);
-        double max = arr[arr.length - 1];
-        double sum = 0;
-
-        for (double value : arr) {
-            sum += (value / max * 100);
-        } //for
-
-        System.out.println(sum / arr.length);
-
+        }
         br.close();
-    } //main
-} //class
+        
+        double sum = 0;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum / arr[arr.length - 1] * 100 / arr.length);
+    }
+}
